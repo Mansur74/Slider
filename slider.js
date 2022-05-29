90a5612844eadbe18e8476472f4820cc5c35deb0
Mansur74 <mansurcildir@hotmail.com>
2022-05-29 16:58:22 +0300

    first

---
+++ slider.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+        	$("#slider").bxSlider({
+			  auto: true,
+			  randomStart: true,
+			  captions: true,
+			  slideWidth: 250,
+			  slideMargin: 10,
+			  speed: 3000,
+			  pager: true,
+			  pagerType: "short",
+			  pagerCustom: "#pager"
+			});
+
+    	});
